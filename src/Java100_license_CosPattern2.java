c7c1c4da7cbf82764f61de744a619f1b5a4a3b19
seungha <frazzle124@gmail.com>
2022-02-13 00:11:12 +0900

    이중 반복문

--- src/Java100_license_CosPattern2.java
+++ src/Java100_license_CosPattern2.java
@@ -6,23 +6,21 @@
 
 public class Java100_license_CosPattern2 {
 	public static void main(String[] args) {
-		
-		// [1] : 변수 선언
-		int number = 1;
-		
-		// [2] : 반복문 돌면서 홀수인지 체크 --> 홀수면 Pass(continue)
-		while( number <= 30 ) {
-			if( number % 2 != 0 ) {
-				number++;
+
+		int i = 1;
+		while (i <= 30){ // 30이하
+			if (i % 2 != 0){
+				i++;
 				continue;
-			}
-			System.out.print( number + " " );
-			number++;
+			} // 짝수면
+			System.out.println(i);
+			i++;
 		}
-		System.out.println();
-		
 	}
 }
+
+
+
 
 
 
